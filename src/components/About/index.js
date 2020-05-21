bac96549a7f7ed985fa9ee0c8645b947bb941573
Cynthia Dominguez <cynthia.dm1216@gmail.com>
2020-05-21 11:34:23 -0500

    updated my About folder

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './style.css';
+import myImg from '../../assets/My_headshot.jpg'
 
 function About() {
     return (
         <div className='aboutWrapper'>
             <div className='uk-tile aboutSection' uk-height-viewport='expand: true'>
-                <h2 className='uk-text-center'>About</h2>
-                <img src='../../assets/My_headshot.jpg' alt='My Picture' className='uk-border-circle myImg' uk-img='true' />
+                <h2 className='uk-text-center About'>About</h2>
+                <img src={myImg} alt='My Picture' className='uk-border-circle MyImg' uk-img='true' />
                 <p className='uk-text-center aboutMe'>
                     Front-End Web Developer with a certificate in Full Stack Web Developer from the University of Texas.
                     With skills in HTML,CSS,JS,Node and Express.This coding bootcamp has helped me in collaborating 
