0c976609ad53ffa670e471442034bac9b1136d25
Cynthia Dominguez <cynthia.dm1216@gmail.com>
2020-05-22 12:33:10 -0500

    updating About me import

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,7 +9,7 @@ function About() {
                 <h2 className='uk-text-center About'>About</h2>
                 <img src={myImg} alt='' className='uk-border-circle MyImg' uk-img='true' />
                 <p className='uk-text-center aboutMe'>
-                    Front-End Web Developer with a certificate in Full Stack Web Developer from the University of Texas.
+                    Have a certificate in Full Stack Web Developer from the University of Texas.
                     With skills in HTML,CSS,JS,Node and Express.This coding bootcamp has helped me in collaborating 
                     with individuals with different backgrounds and has thought me how to calloborate and manage my time. Having the oppurtunity to take this bootcamp
                     has let me enjoy and learn how to create a website from scratch and see it come to life. I look forward to
