a8d69f29a00cae51b6d5d6735cc02828546599ac
Cynthia Dominguez <cynthia.dm1216@gmail.com>
2020-05-23 01:15:52 -0500

    deleting files that werent needed

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import './style.css'
-// import ContactCard from '../components/ContactCard';
-import MyInfo from '../components/MyInfo';
+import ContactForm from '../components/ContactForm';
 
 function Contact(){
     return(
         <div className='contactWrapper'>
             <h2 className='uk-text-center Contact'>Contact</h2>
             <div className='contactContainer uk-child-width-1-2@m' uk-grid='true'>
-                {/* <ContactCard /> */}
-                <MyInfo />
+                <ContactForm />
             </div>
         </div>
     );
