34a89d1ac655b92af66821a53153f645872e56a7
Cynthia Dominguez <cynthia.dm1216@gmail.com>
2020-05-22 23:49:30 -0500

    cleaning up code

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,7 +5,6 @@ import myImg from '../../assets/My_headshot.jpg'
 function About() {
     return (
         <div className='aboutWrapper'>
-              {/* <div className='uk-tile aboutSection'>   */}
                 <h2 className='uk-text-center About'>About</h2>
                  <img src={myImg} alt='' className='uk-border-circle MyImg' uk-img='true' /> 
                 <p className='uk-text-center aboutMe'>
@@ -15,10 +14,8 @@ function About() {
                     has let me enjoy and learn how to create a website from scratch and see it come to life. I look forward to
                     see how much more I learn through my journey in coding and provide the best for the users.
                 </p>
-                {/* </div>    */}
                 <a href="https://docs.google.com/document/d/1kRt1syrTPc4EsuekK8yoeT1-0yv_afnfkYeyxyhm6eg/edit?usp=sharing" target="_blank" rel='noopener noreferrer'>Resume</a>
               </div> 
-        //  </div>
     );
 }
 
